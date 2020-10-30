2334d70f5504b49b4290585e64e0c664722149be
k0wia <konstantine.arsenashvili.1@btu.edu.ge>
2020-10-31 02:50:22 +0400

    update

--- src/main new.kt
+++ src/main new.kt
@@ -1,9 +1,37 @@
+import kotlin.math.min
+import kotlin.math.roundToInt
+
 fun main(){
     var f1 = Fraction(3F, 4F)
 
     var f2 = Fraction(6F, 8F)
     println(f1 == f2)
     println(f1)
+    println(f2)
+    f2.shekveca()
+    println(f2)
+
+    println(f1.gamravleba(f2))
+    println(f1.mimateba(f2))
+
+
+
+
+
+}
+fun usg(a : Float, b: Float): Float {
+
+    var c = min(a,b)
+    var usg1: Float = 0F
+
+    for(i in c.toInt() downTo 1){
+
+        if(a.toInt()%i==0 && b.toInt()%i==0){
+            usg1=i.toFloat()
+            break
+        }
+    }
+    return usg1
 
 }
 
@@ -24,6 +52,31 @@ class Fraction(n: Float, d: Float) {
         return "$numerator/$denominator"
     }
 
+    fun shekveca(){
+        var d:Float = usg(numerator,denominator)
+        numerator = numerator/d
+        denominator = denominator/d
+    }
+
+    fun gamravleba(other:Fraction):Fraction{
+        var newMricxveli: Float = numerator*other.numerator
+        var newMnishvneli: Float = denominator*other.denominator
+        var newWiladi:Fraction = Fraction(newMricxveli, newMnishvneli)
+        return newWiladi
+    }
+
+    fun mimateba(other : Fraction):Fraction{
+        var usj:Float= (denominator*other.denominator)/usg(denominator, other.denominator)
+
+        var newMricxveli: Float = ((numerator*(usj/denominator))+(other.numerator*(usj/other.denominator)))
+        var newWiladi: Fraction = Fraction(newMricxveli,usj)
+        return newWiladi
+    }
 
 
 }
+
+
+
+
+
